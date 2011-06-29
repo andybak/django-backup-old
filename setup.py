e726f024811737892e55ba6f856bb5224204f2e9
Andy Baker <andy@andybak.net>
2011-06-29 00:40:54 -0700

    Yikes! Unescaped apostrophe.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'django_backup',
     packages = find_packages(),
     version = '1.0.1',
-    description = 'A fork and extension of Dmitriy Kovalev's backup database command for django.',
+    description = 'A fork and extension of Dmitriy Kovalev\'s backup database command for django.',
     author = 'Dmitriy Kovalev, Michael Huynh, msaelices, Andy Baker, Chen Zhe',
     #author_email = '',
     url = 'http://github.com/andybak/django-backup',
