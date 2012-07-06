3c88fea27cc20d99f547f69ae3f7c5a5dd158e49
Artem Rizhov <artem.rizhov@gmail.com>
2012-07-06 19:24:58 +0300

    Configured media root

--- test_project/core/settings.py
+++ test_project/core/settings.py
@@ -51,7 +51,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
