41f61344c6af7f6bc4cb1bcf543c1980b4769093
Artem Rizhov <artem.rizhov@gmail.com>
2012-07-06 19:14:39 +0300

    Added django_backup app to test project

--- test_project/core/settings.py
+++ test_project/core/settings.py
@@ -122,6 +122,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'django_backup',
 )
 
 # A sample logging configuration. The only tangible logging
