12a946533b9d57521e225428e472e337de50ff08
Artem Rizhov <artem.rizhov@gmail.com>
2012-07-06 20:17:47 +0300

    Reconfigured db connection to use mysql

--- test_project/core/settings.py
+++ test_project/core/settings.py
@@ -1,5 +1,8 @@
 # Django settings for test_project project.
+# Don't change this file for local needs. Create local_settings.py instead.
+
 import os
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -14,8 +17,8 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'), # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -157,3 +160,11 @@ LOGGING = {
         },
     }
 }
+
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
