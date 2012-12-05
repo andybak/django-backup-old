621d391a0247e4c40b193701546a38c38cfbc0cf
fruitschen <fruitschen@gmail.com>
2012-12-05 11:48:31 +0800

    add rsync option

--- django_backup/management/commands/backup.py
+++ django_backup/management/commands/backup.py
@@ -15,6 +15,7 @@ import pysftp as ssh
 
 TIME_FORMAT = '%Y%m%d-%H%M%S'
 regex = re.compile(r'(\d){8}-(\d){6}')
+GOOD_RSYNC_FLAG = '__good_backup'
 
 def is_db_backup(filename):
     return filename.startswith('backup_')
@@ -103,10 +104,14 @@ class Command(BaseCommand):
             help='Destination Directory'),
         make_option('--media', '-m', action='store_true', default=False, dest='media',
             help='Backup media dir'),
+        make_option('--rsync', '-r', action='store_true', default=False, dest='rsync',
+            help='Backup media dir with rsync'),
         make_option('--cleandb', action='store_true', default=False, dest='clean_db',
             help='Clean up surplus database backups'),
         make_option('--cleanmedia', action='store_true', default=False, dest='clean_media',
             help='Clean up surplus media backups'),
+        make_option('--cleanrsync', action='store_true', default=False, dest='clean_rsync',
+            help='Clean up broken rsync backups'),
         make_option('--nolocal', action='store_true', default=False, dest='no_local',
             help='Reserve local backup or not'),
         make_option('--deletelocal', action='store_true', default=False, dest='delete_local',
@@ -119,6 +124,10 @@ class Command(BaseCommand):
             help='Clean up surplus local media backups'),
         make_option('--cleanremotemedia', action='store_true', default=False, dest='clean_remote_media',
             help='Clean up surplus remote media backups'),
+        make_option('--cleanlocalrsync', action='store_true', default=False, dest='clean_local_rsync',
+            help='Clean up local broken rsync backups'),
+        make_option('--cleanremotersync', action='store_true', default=False, dest='clean_remote_rsync',
+            help='Clean up remote broken rsync backups'),
     )
     help = "Backup database. Only Mysql and Postgresql engines are implemented"
 
@@ -131,13 +140,17 @@ class Command(BaseCommand):
         self.compress = options.get('compress')
         self.directories = options.get('directories')
         self.media = options.get('media')
+        self.rsync = options.get('rsync')
         self.clean = options.get('clean')
         self.clean_db = options.get('clean_db')
         self.clean_media = options.get('clean_media')
+        self.clean_rsync = options.get('clean_rsync')
         self.clean_local_db = options.get('clean_local_db')
         self.clean_remote_db = options.get('clean_remote_db')
         self.clean_local_media = options.get('clean_local_media')
         self.clean_remote_media = options.get('clean_remote_media')
+        self.clean_local_rsync = options.get('clean_local_rsync')
+        self.clean_remote_rsync = options.get('clean_remote_rsync')
         self.no_local = options.get('no_local')
         self.delete_local = options.get('delete_local')
 
@@ -165,6 +178,19 @@ class Command(BaseCommand):
         self.ftp_username = settings.BACKUP_FTP_USERNAME
         self.ftp_password = settings.BACKUP_FTP_PASSWORD
 
+
+        if self.clean_rsync:
+            print 'cleaning broken rsync backups'
+            self.clean_broken_rsync()
+        else:
+            if self.clean_local_rsync:
+                print 'cleaning local broken rsync backups'
+                self.clean_local_broken_rsync()
+
+            if self.clean_remote_rsync:
+                print 'cleaning remote broken rsync backups'
+                self.clean_remote_broken_rsync()
+        
         if self.clean_db:
             print 'cleaning surplus database backups'
             self.clean_surplus_db()
@@ -219,12 +245,17 @@ class Command(BaseCommand):
         # Backing up directories
         dir_outfiles = []
 
-        # Backup all the directories in one file. 
-        if self.directories:
+
+        if self.directories: # We need to do media backup
             all_directories = ' '.join(self.directories)
-            all_outfile = os.path.join(self.backup_dir, 'dir_%s.tar.gz' % (self._time_suffix()))
-            self.compress_dir(all_directories, all_outfile)
-            dir_outfiles.append(all_outfile)
+            self.all_directories = all_directories
+            if self.rsync:
+                self.do_media_rsync_backup()
+            else:
+                # Backup all the directories in one file. 
+                all_outfile = os.path.join(self.backup_dir, 'dir_%s.tar.gz' % (self._time_suffix()))
+                self.compress_dir(all_directories, all_outfile)
+                dir_outfiles.append(all_outfile)
 
         # Sending mail with backups
         if self.email:
@@ -403,7 +434,7 @@ class Command(BaseCommand):
             if remove_all:
                 print '=' * 70
                 print 'cleaning up local media backups'
-                command = 'rm %s' % remove_all
+                command = 'rm -r %s' % remove_all
                 print '=' * 70
                 print 'Running Command: %s' % command
                 os.system(command)
@@ -425,7 +456,7 @@ class Command(BaseCommand):
             if remove_all_remote:
                 print '=' * 70
                 print 'cleaning up remote media backups'
-                command = 'rm %s' % remove_all_remote
+                command = 'rm -r %s' % remove_all_remote
                 print '=' * 70
                 print 'Running Command on remote server: %s' % command
                 sftp.execute(command)
@@ -433,4 +464,87 @@ class Command(BaseCommand):
         except ImportError:
             print 'cleaned nothing, because BACKUP_MEDIA_COPIES is missing'
 
+
+    def do_media_rsync_backup(self):
+        #local media rsync backup
+        if not self.delete_local and not self.no_local:
+            print 'Doing local media rsync backup'
+            local_current_backup = os.path.join(self.backup_dir, 'current')
+            local_backup_target = os.path.join(self.backup_dir, 'dir_%s' % (self._time_suffix()))
+            local_info = {
+                'local_current_backup': local_current_backup,
+                'all_directories': self.all_directories,
+                'local_backup_target': local_backup_target,
+                'rsync_flag': GOOD_RSYNC_FLAG,
+            }
+            local_rsync_cmd = 'rsync -az --link-dest=%(local_current_backup)s %(all_directories)s %(local_backup_target)s' % local_info
+            local_mark_cmd = 'touch %(local_backup_target)s/%(rsync_flag)s' % local_info
+            local_link_cmd = 'rm -f %(local_current_backup)s && ln -s %(local_backup_target)s %(local_current_backup)s' % local_info
+            cmd = '\n'.join(['%s&&%s' % (local_rsync_cmd, local_mark_cmd), local_link_cmd])
+            print cmd
+            os.system(cmd)
+
+
+        #remote media rsync backup
+        if self.ftp:
+            print 'Doing remote media rsync backup'
+            host = '%s@%s' % (self.ftp_username, self.ftp_server)
+            remote_current_backup = os.path.join(self.remote_dir, 'current')
+            remote_backup_target = os.path.join(self.remote_dir, 'dir_%s' % (self._time_suffix()))
+            remote_info = {
+                'remote_current_backup': remote_current_backup,
+                'all_directories': self.all_directories,
+                'host': host,
+                'remote_backup_target': remote_backup_target,
+                'rsync_flag': GOOD_RSYNC_FLAG,
+            }
+            remote_rsync_cmd = 'rsync -az --link-dest=%(remote_current_backup)s %(all_directories)s %(host)s:%(remote_backup_target)s' % remote_info
+            remote_mark_cmd = 'ssh %(host)s "touch %(remote_backup_target)s/%(rsync_flag)s"' % remote_info
+            remote_link_cmd = 'ssh %(host)s "rm -f %(remote_current_backup)s && ln -s %(remote_backup_target)s %(remote_current_backup)s"' % remote_info
+            cmd = '\n'.join(['%s&&%s' % (remote_rsync_cmd, remote_mark_cmd), remote_link_cmd])
+            print cmd
+            sftp = self.get_connection()
+            try:
+                sftp.mkdir(self.remote_dir)
+            except IOError:
+                pass
+            os.system(cmd)        
+
+    def clean_broken_rsync(self):
+        self.clean_local_broken_rsync()
+        self.clean_remote_broken_rsync()
+
+    def clean_remote_broken_rsync(self):
+        sftp = self.get_connection()
+        backups = [i.strip() for i in sftp.execute('ls %s' % self.remote_dir)]
+        backups = filter(is_media_backup, backups)
+        backups.sort()
+        commands = []
+        for backup in backups:
+            #find the GOOD_RSYNC_FLAG file in the backup dir
+            backup_path = os.path.join(self.remote_dir, backup)
+            flag_file = os.path.join(backup_path, GOOD_RSYNC_FLAG)
+            cmd = 'test -e %s||rm -rf %s' % (flag_file, backup_path)
+            commands.append(cmd)
+
+        full_cmd = '\n'.join(commands)
+        print full_cmd
+        sftp.execute(full_cmd)
+        sftp.close()
+
+    def clean_local_broken_rsync(self):
+        # local(web server)
+        backups = os.listdir(self.backup_dir)
+        backups = filter(is_media_backup, backups)
+        backups.sort()
+        commands = []
+        for backup in backups:
+            #find the GOOD_RSYNC_FLAG file in the backup dir
+            backup_path = os.path.join(self.backup_dir, backup)
+            flag_file = os.path.join(backup_path, GOOD_RSYNC_FLAG)
+            cmd = 'test -e %s||rm -rf %s' % (flag_file, backup_path)
+            commands.append(cmd)
+        full_cmd = '\n'.join(commands)
+        print full_cmd
+        os.system(full_cmd)
 
